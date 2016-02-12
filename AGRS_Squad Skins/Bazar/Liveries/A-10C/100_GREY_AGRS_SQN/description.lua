eb661bc4b347c2147e971bfe25673cafe8ec5e57
joelklingler <joel.klingler@hotmail.com>
2016-02-12 15:16:06 +0100

    Added skins
    Added AGRS_Squad Skins by Raptor (Jason)

---
+++ AGRS_Squad Skins/Bazar/Liveries/A-10C/100_GREY_AGRS_SQN/description.lua
@@ -0,0 +1,20 @@
+livery = {
+	{"A-10C_PAINT_1-a", 0 ,"punisher-a",false};
+	{"A-10C_PAINT_1-b", 0 ,"punisher-b",false};
+	{"A-10C_PAINT_1-c", 0 ,"punisher-c",false};
+	{"A-10C_PAINT_1-d", 0 ,"punisher-d",false};
+	{"A-10C_PAINT_1-e", 0 ,"punisher-e",false};
+	{"A-10C_PAINT_1-f", 0 ,"punisher-f",false};
+	{"A-10C_PAINT_1-g", 0 ,"punisher-g",false};
+	{"A-10C_PAINT_1-h", 0 ,"punisher-h",false};
+	{"A-10C_PAINT_1-i", 0 ,"punisher-i",false};
+	{"A-10C_PAINT_1-j", 0 ,"punisher-j",false};
+	{"A-10C_PAINT_1-k", 0 ,"punisher-k",false};
+	{"A-10C_PAINT_1-L", 0 ,"punisher-L",false};
+	{"A-10_Number", 0 ,"TactNumbers-USAF-Light_black",true};
+	{"A-10_Number_Noze_F", 0 ,"TactNumbers-USAF-Light_black",true};
+	{"A-10_Number_Noze_T", 0 ,"empty",true};
+	{"A-10_Number_Wheel", 0 ,"empty",true};
+
+}
+countries = {"USA"}
---
+++ AGRS_Squad Skins/Bazar/Liveries/F-15C/100_SBLACK_AGRS_SQN/description.lua
@@ -0,0 +1,23 @@
+livery = {
+	{"f15nose", 0 ,"f15_nose",false};
+	{"f15nose", 3 ,"f15_decol_empty",true};
+	{"f15bottom", 0 ,"f15_bottom",false};
+	{"f15bottom", 2 ,"f15_bottom_spec",false};
+	{"f15bottom", 3 ,"f15_decol_empty",true};
+	{"f15centr", 0 ,"f15_centr",false};
+	{"f15wingl", 0 ,"f15_wing_l",false};
+	{"f15wingl", 3 ,"f15_decol_empty",true};
+	{"f15wingr", 0 ,"f15_wing_r",false};
+	{"f15stab", 0 ,"f15_stab_v",false};
+	{"f15stab", 3 ,"f15_decol_empty",true};
+	{"f15numbers", 0 ,"f15_numbers",true};
+	{"pilot_F15_patch", 0 ,"pilot_f15_patch_AGRS_sqdn",false};
+	{"pilot_F15_00_body", 0 ,"pilot_f15_00_a",true};
+	{"f15PTB", 0 ,"f15_PTB",false};
+	{"f15PTB", 2 ,"f15_ptb_spec",false};
+	
+
+}
+
+countries = {"USA"}
+
